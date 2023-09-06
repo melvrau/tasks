09d6725854336bd19bf18b6aee78275c16d58878
Melvin Rau <melvrau@udel.edu>
2023-09-05 20:15:10 -0400

    Added hello world text

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,9 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript - Melvin Rau
+                UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <p>Melvin Rau - Hello World</p>
         </div>
     );
 }
