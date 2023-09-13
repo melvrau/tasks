9e4beb091f760bef05ee964857a832063337488c
Melvin Rau <melvrau@udel.edu>
2023-09-13 17:12:59 -0400

    completed convertYesNo

--- src/functions.ts
+++ src/functions.ts
@@ -48,5 +48,12 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word === "yes" || word === "YES") {
+        return true;
+    }
+    if (word === "no" || word === "NO") {
+        return false;
+    } else {
+        return null;
+    }
 }
