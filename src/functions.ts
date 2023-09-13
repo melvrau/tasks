fd1709fda1d2981bc636a8714b4d772eb075132b
Melvin Rau <melvrau@udel.edu>
2023-09-13 16:52:32 -0400

    completed add3

--- src/functions.ts
+++ src/functions.ts
@@ -13,7 +13,17 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let ans = 0;
+    if (first > 0) {
+        ans = ans + first;
+    }
+    if (second > 0) {
+        ans = ans + second;
+    }
+    if (third > 0) {
+        ans = ans + third;
+    }
+    return ans;
 }
 
 /**
